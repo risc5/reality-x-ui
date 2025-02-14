59ad6021f69d61114681fff831815203fe4bb3ee
Alexander <xalexu@gmail.com>
2025-02-14 19:38:03 +0800

    final

--- install.sh
+++ install.sh
@@ -132,6 +132,7 @@ install_x-ui() {
     chmod +x x-ui bin/xray-linux-${arch}
     cp -f x-ui.service /etc/systemd/system/
     #wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/FranzKafkaYu/x-ui/main/x-ui.sh
+    cp x-ui.sh /usr/bin/x-ui
     chmod +x /usr/local/x-ui/x-ui.sh
     chmod +x /usr/bin/x-ui
     config_after_install
